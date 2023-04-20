52209230e08c3e31a78fdb53a8c97c3a8d133591
brunoprosa <bruno.prosa01@gmail.com>
2023-04-20 15:13:39 -0300

    pull1

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+axios.defaults.headers.common['Authorization'] = 'QvrjsZKGJ1H8evjKA56QR65s';
+
+
 function showScreen1() {
     document.querySelector('#screen1').classList.remove('escondido');
     document.querySelector('#screen2').classList.add('escondido');
     document.querySelector('#screen3').classList.add('escondido');
+    document.querySelector('#screen34').classList.add('escondido');
   }
   
   function showScreen2() {
@@ -14,6 +18,7 @@ function showScreen1() {
     document.querySelector('#screen1').classList.add('escondido');
     document.querySelector('#screen2').classList.add('escondido');
     document.querySelector('#screen3').classList.remove('escondido');
+    document.querySelector('#screen31').classList.remove('escondido');
   }
 
   function showScreen32(){
@@ -25,7 +30,7 @@ function showScreen1() {
     document.querySelector('#screen32').classList.add('escondido');
     document.querySelector('#screen33').classList.remove('escondido');
   }
-  
+
   function showScreen34(){
     document.querySelector('#screen33').classList.add('escondido');
     document.querySelector('#screen34').classList.remove('escondido');
